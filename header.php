6f1b9863b9f07bb1e4c768608b93e76e82520eac
TomRemond <remondtom@gmail.com>
2019-03-08 11:27:06 +0100

    page accueil

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
     <html lang="fr">
         <head>
             <meta charset="UTF-8">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> 
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
             <link rel="stylesheet" type="text/css" href="./style.css">
         </head>
@@ -9,5 +11,4 @@
     <body>
         <?php
             require_once("./functions.php");
-            session_start();
         ?>
--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 include("header.php");
 ?>
-<div id=topIndex>image de présentation</div>
-
-<div id="descriptionPeintre">
-    <div id="LaptiteTortue"><img src="FranklinInTheHood.png" style="width:160px" /></div>
-    <div id="blabla">Tempore quo primis auspiciis in mundanum fulgorem surgeret victura dum erunt homines Roma, ut augeretur sublimibus incrementis, foedere pacis aeternae Virtus convenit atque Fortuna plerumque dissidentes, quarum si altera defuisset, ad perfectam non venerat summitatem.</div>
+<div id=topIndex class="row grey lighten-2">
+    <h2>Image de présentation</h2>
 </div>
 
-<div>Mes ouevres</div>
+<div class="container">
+    <div class="row" style="text-align:center">
+        <h4>BRYANGELO CRANK</h4>
+        <img src="FranklinInTheHood" alt="FranklinInTheHood">
+    </div>
+    <div class="row">
+    Tempore quo primis auspiciis in mundanum fulgorem surgeret victura dum erunt homines Roma, ut augeretur sublimibus incrementis, foedere pacis aeternae Virtus convenit atque Fortuna plerumque dissidentes, quarum si altera defuisset, ad perfectam non venerat summitatem.
+    </div>
+
+    <div class="container">
+        <div class="row grey lighten-2" style="text-align:center">
+            <h4>Mes ouevres</h4>
+        </div>
+        liste des oeuvre : ici 
+    </div>
+</div>
+    
